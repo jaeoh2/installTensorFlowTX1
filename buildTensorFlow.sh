6e5535dc1c339b20f5188a535a9b8f6b6b855b96
Jaeoh2 <suby38@gmail.com>
2017-01-05 08:21:59 +0900

    Update buildTensorFlow.sh

--- buildTensorFlow.sh
+++ buildTensorFlow.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-# NVIDIA Jetson TX1
+# NVIDIA Gtx 1070
+# Ubuntu 16.04
 # TensorFlow Installation
 # Build Tensorflow
 cd $HOME/tensorflow
-bazel build -c opt --local_resources 3072,4.0,1.0 --verbose_failures --config=cuda //tensorflow/tools/pip_package:build_pip_package
+bazel build -c opt --config=cuda //tensorflow/tools/pip_package:build_pip_package
+
 
