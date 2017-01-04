68378574b75589bdf4dcfa1d5468c2b5636c3eba
Jaeoh2 <suby38@gmail.com>
2017-01-05 08:14:20 +0900

    Update buildBazel.sh

--- scripts/buildBazel.sh
+++ scripts/buildBazel.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-# NVIDIA Jetson TX1
+# Ubuntu 16.04
 # TensorFlow Installation
 # Build Bazel
-INSTALL_DIR=$PWD
-cd $HOME
-git clone https://github.com/bazelbuild/bazel.git
-cd bazel
-git checkout 0.3.2
-cp ../protobuf/src/protoc third_party/protobuf/protoc-linux-arm32.exe
-cp ../grpc-java/compiler/build/exe/java_plugin/protoc-gen-grpc-java third_party/grpc/protoc-gen-grpc-java-0.15.0-linux-arm32.exe
-patch -p1 < $INSTALL_DIR/patches/bazel.patch
-./compile.sh 
-sudo cp output/bazel /usr/local/bin
+# You might need to
+sudo apt-get install software-properties-common -y
+# Add Bazel distribution URI as a package source (one time setup)
+echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
+# Install and update Bazel
+sudo apt-get update && sudo apt-get install bazel -y
+
