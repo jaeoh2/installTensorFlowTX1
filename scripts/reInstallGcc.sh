0d266288b99a3ddd916281002514ec3af3b52f1c
Jaeoh2 <suby38@gmail.com>
2017-01-05 08:17:03 +0900

    Update reInstallGcc.sh
    fix typo

--- scripts/reInstallGcc.sh
+++ scripts/reInstallGcc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Re-install to order version of gcc,g++
+#Re-install to older version of gcc,g++
 sudo apt-get install gcc-4.8
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 10
 sudo apt-get install g++-4.8
