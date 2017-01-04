76450bb9a09f5bea921446262f435a7250b0f581
Jaeoh2 <suby38@gmail.com>
2017-01-04 16:24:58 +0900

    Update installDependencies.sh
    install latest nvidia-driver

--- scripts/installDependencies.sh
+++ scripts/installDependencies.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-# NVIDIA Jetson TX1
+# Ubuntu 16.04
 # Install TensorFlow dependencies
-# Install Java
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get update
-sudo apt-get install oracle-java8-installer
+# Install latest nvidia-driver from ppa:graphics-drivers/ppa
+sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo apt-get install nvidia-375
+sudo apt-get install mesa-common-dev
+sudo apt-get install freeglut3-dev
 # Install other dependencies
 sudo apt-get install zip unzip autoconf automake libtool curl zlib1g-dev maven -y
 sudo apt-get install python-numpy swig python-dev python-pip python-wheel -y
