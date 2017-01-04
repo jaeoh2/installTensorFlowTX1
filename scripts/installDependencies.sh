efc5bd485b97c8f920c18f157fa25bc70e101191
Jaeoh2 <suby38@gmail.com>
2017-01-05 08:04:53 +0900

    Update installDependencies.sh

--- scripts/installDependencies.sh
+++ scripts/installDependencies.sh
@@ -6,6 +6,10 @@ sudo add-apt-repository ppa:graphics-drivers/ppa
 sudo apt-get install nvidia-375
 sudo apt-get install mesa-common-dev
 sudo apt-get install freeglut3-dev
+# Install Java
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
 # Install other dependencies
-sudo apt-get install zip unzip autoconf automake libtool curl zlib1g-dev maven -y
+sudo apt-get install build-essential -y
 sudo apt-get install python-numpy swig python-dev python-pip python-wheel -y
