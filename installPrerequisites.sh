24a8a6e33574815c1747d8431e59d32d56c54273
Jaeoh Lee <suby38@gmail.com>
2017-09-04 11:00:43 +0900

    Update installPrerequisites.sh

--- installPrerequisites.sh
+++ installPrerequisites.sh
@@ -3,6 +3,8 @@
 # Install TensorFlow dependencies and prerequisites
 # Install Java and other dependencies by apt-get
 ./scripts/installDependencies.sh
+# Install cuda Toolkit
+./scripts/installCudaToolKit.sh
 # Re-install older version of gcc/g++
 ./scripts/reInstallGcc.sh
 # Install Bazel build
